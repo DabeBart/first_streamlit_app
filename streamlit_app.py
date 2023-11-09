473c4d23779fa44a9f5121ac00047876d6ad5ecb
DabeBart <150330104+DabeBart@users.noreply.github.com>
2023-11-09 17:21:14 +0800

    Update streamlit_app.py
    Build your own fruit smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,7 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header(':bowl_with_spoon: Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text(':chicken: Hard-Boiled Free-Range Egg')
+Streamlit.text(':avocado: Avocado Toast')
+
+streamlit.header(':banana: :strawberry: Build Your Own Fruit Smoothie 	:kiwifruit: :grapes:')
