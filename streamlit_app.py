c6f17757a5ddc1528eb4f250d039e29343776e3f
DabeBart <150330104+DabeBart@users.noreply.github.com>
2023-11-13 21:53:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,14 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header(':bowl_with_spoon: Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.header(:chicken:) streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text(:avocado: 'Avocado Toast')
 
 streamlit.header(':banana: :strawberry: Build Your Own Fruit Smoothie 	:kiwifruit: :grapes:')
 
 
 import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
