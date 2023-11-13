c9deb7f3054d3dfd317dc62fbf13e2957ad924fb
DabeBart <150330104+DabeBart@users.noreply.github.com>
2023-11-13 21:51:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header(':bowl_with_spoon: Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text(:chicken: 'Hard-Boiled Free-Range Egg')
+streamlit.header(:chicken:) streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text(:avocado: 'Avocado Toast')
 
 streamlit.header(':banana: :strawberry: Build Your Own Fruit Smoothie 	:kiwifruit: :grapes:')
