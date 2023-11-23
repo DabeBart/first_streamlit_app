e042e8ab18c0d214e901b176a89626cb564b5b20
DabeBart <150330104+DabeBart@users.noreply.github.com>
2023-11-23 16:36:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,3 +53,15 @@ create or replace TABLE PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST (
 	FRUIT_NAME VARCHAR(25)
 );
 
+
+insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST
+values ('banana')
+, ('cherry')
+, ('strawberry')
+, ('pineapple')
+, ('apple')
+, ('mango')
+, ('coconut')
+, ('plum')
+, ('avocado')
+, ('starfruit');
